136063224ae6f7806f1feed3cd5f113852bafd8b
Nathan Bertaud <nathan.bertaud@ynov.com>
2023-09-02 19:54:18 +0200

    add db cnnection

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
 
 const port = ":8080"
 
@@ -22,6 +31,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	dsn := "host=localhost port=5433 dbname=discode-user user=postgres password=admin sslmode=prefer connect_timeout=10" //Postgresql server connection string
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println(db)
 	mux := http.NewServeMux()
 	cssFileServer := http.FileServer(http.Dir("CSS"))
 	mux.Handle("/CSS/", http.StripPrefix("/CSS/", cssFileServer))
